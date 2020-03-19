d9ccae537bc34d8292c1ac5caa8d08d919794561
danikmartyniuk <daniilmartyniuk.sbmt@gmail.com>
2020-03-19 23:50:24 +0300

    improved tests

--- src/test/java/tests/PhonesTest.java
+++ src/test/java/tests/PhonesTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class PhonesTest extends BaseTest {
 
-    @Test(priority = 1, description = "Getting samsung phones average price")
+    @Test(retryAnalyzer = RetryFailedTestCases.class, priority = 1, description = "Getting samsung phones average price")
     public void samsungPrice() {
         mainSteps.openPhonePrices();
         phoneSteps.chooseSamsung();
@@ -16,7 +16,7 @@ public class PhonesTest extends BaseTest {
         phoneSteps.chooseApple();
     }
 
-    @Test(priority = 2, description = "Getting Xiaomi (China) phones average price")
+    @Test(retryAnalyzer = RetryFailedTestCases.class, priority = 2, description = "Getting Xiaomi (China) phones average price")
     public void xiaomiPrice() {
         mainSteps.openPhonePrices();
         phoneSteps.chooseXiaomi();
